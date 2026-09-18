24ade4443caaf3a8ac95fb689402c48be933338a
MpumeleloBaatjie <ST10517925@rcconnect.edu.za>
2026-09-18 16:04:18 +0200

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author baatj
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    /**
+     * Test of checkUserName method, of class Login.
+     */
+    @Test
+    public void testCheckUserName() {
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+    /**
+     * Test of checkCellPhoneNumber method, of class Login.
+     */
+    @Test
+    public void testCheckCellPhoneNumber() {
+    }
+
+    /**
+     * Test of registerUser method, of class Login.
+     */
+    @Test
+    public void testRegisterUser() {
+    }
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+    }
+
+    /**
+     * Test of returnLoginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnLoginStatus() {
+    }
+
+    /**
+     * Test of getRegisteredUser method, of class Login.
+     */
+    @Test
+    public void testGetRegisteredUser() {
+    }
+    
+}
---
+++ UserTest.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author baatj
+ */
+public class UserTest {
+    
+    public UserTest() {
+    }
+
+    /**
+     * Test of getFirstName method, of class User.
+     */
+    @Test
+    public void testGetFirstName() {
+    }
+
+    /**
+     * Test of setFirstName method, of class User.
+     */
+    @Test
+    public void testSetFirstName() {
+    }
+
+    /**
+     * Test of getLastName method, of class User.
+     */
+    @Test
+    public void testGetLastName() {
+    }
+
+    /**
+     * Test of setLastName method, of class User.
+     */
+    @Test
+    public void testSetLastName() {
+    }
+
+    /**
+     * Test of getUsername method, of class User.
+     */
+    @Test
+    public void testGetUsername() {
+    }
+
+    /**
+     * Test of setUsername method, of class User.
+     */
+    @Test
+    public void testSetUsername() {
+    }
+
+    /**
+     * Test of getPassword method, of class User.
+     */
+    @Test
+    public void testGetPassword() {
+    }
+
+    /**
+     * Test of setPassword method, of class User.
+     */
+    @Test
+    public void testSetPassword() {
+    }
+
+    /**
+     * Test of getCellPhoneNumber method, of class User.
+     */
+    @Test
+    public void testGetCellPhoneNumber() {
+    }
+
+    /**
+     * Test of setCellPhoneNumber method, of class User.
+     */
+    @Test
+    public void testSetCellPhoneNumber() {
+    }
+    
+}
